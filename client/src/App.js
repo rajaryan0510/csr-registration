56a976b4f0271d63cf025ed7f253dba6cdb00624
rajaryan25 <190101120116@cutm.ac.in>
2022-09-18 20:49:30 +0530

    CSR Card

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className='bg-[#ffffff]'>
       
     <Navbar/>
-    <Login/>
+    {/* <Login/> */}
     <Dashboard/>
    
     
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,51 +1,55 @@
-import { Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material';
+import { Card, CardActionArea, CardContent, Typography } from '@mui/material';
 import React from 'react';
 import CulturalIMG from '../assets/cultural.jpg'
+import SportsIMG from '../assets/sport.jpg'
+import ResponsibilitiesIMG from '../assets/responsibility.jpg'
 
 export default function Dashboard() {
     return (
-        <div className='flex mx-[100px] ' >
+        <div className='flex px-[100px] bg-yellow-300 ' >
 
-        <Card className='pt-[150px] ml-[50px] ' sx={{ maxWidth: 345 }}>
+        <Card className='my-[150px] mx-[50px] ' sx={{ maxWidth: 345 }}>
             <CardActionArea>
-                <img className='ml-[30px]  w-[280px] h-[140px] object-cover  ' src={CulturalIMG} alt='cutm'/>
+                <img className='m-[30px]  w-[280px] h-[140px] object-cover rounded-lg  ' src={CulturalIMG} alt='cutm'/>
                 <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
-                    cultural
+                    Cultural
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                    Lizards are a widespread group of squamate reptiles, with over 6,000
-                    species, ranging across all continents except Antarctica
+                    Work on CUTM students Newsletter, Wall Magazine, and Annual Magazine. Organising/Participating in dance, music or drama
+                    events. Preparing in any event organised by the clubs under cultural activities.
+                    Painting and Photography etc.......
                 </Typography>
                 </CardContent>
             </CardActionArea>
             </Card>
 
-            <Card className='pt-[150px] ml-[50px] ' sx={{ maxWidth: 345 }}>
+            <Card className='my-[150px] mx-[50px] ' sx={{ maxWidth: 345 }}>
             <CardActionArea>
-                <img className='ml-[30px]  w-[280px] h-[140px] object-cover  ' src={CulturalIMG} alt='cutm'/>
+                <img className='m-[30px]  w-[280px] h-[140px] object-cover rounded-lg ' src={SportsIMG} alt='cutm'/>
                 <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
                     Sport
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                    Lizards are a widespread group of squamate reptiles, with over 6,000
-                    species, ranging across all continents except Antarctica
+                    Participating in individual and team sports of the Institute. Taking part in Health club/ Ypga Club. Participating / 
+                    organising any adventures activities. Organising a college marathon/other sporting event etc........ 
                 </Typography>
                 </CardContent>
             </CardActionArea>
             </Card>
 
-            <Card className='pt-[150px] ml-[50px] ' sx={{ maxWidth: 345 }}>
+            <Card className='my-[150px] mx-[50px] ' sx={{ maxWidth: 345 }}>
             <CardActionArea>
-                <img className='ml-[30px]  w-[280px] h-[140px] object-cover  ' src={CulturalIMG} alt='cutm'/>
+                <img className='m-[30px]  w-[280px] h-[140px] object-cover rounded-lg ' src={ResponsibilitiesIMG} alt='cutm'/>
                 <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
                 Responsibility
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                    Lizards are a widespread group of squamate reptiles, with over 6,000
-                    species, ranging across all continents except Antarctica
+                    Any activities which benefit the community or environment. Plantation inside or outside CUTM. Visiting and helping in 
+                    hospitals or orphanages. Blood Donation Camps. Organising Health Check up Camps. Save environment from pollution Camps.
+                    Educational camp for population control. Youth Red Cross activities. Fund raising for social/environmental projects etc......
                 </Typography>
                 </CardContent>
             </CardActionArea>
